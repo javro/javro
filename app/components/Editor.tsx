dff25e31389d63538c5d00f966b084093a400a28
Baptiste Lecocq <baptiste.lecocq@gmail.com>
2020-05-15 12:34:47 +0200

    feat: add ant

--- app/components/Editor.tsx
+++ app/components/Editor.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { Col, Layout, Menu, Row } from 'antd';
+import styles from './Editor.css';
+
+const { Header, Content } = Layout;
 
 type Props = {
   avro: {
@@ -12,23 +16,32 @@ type Props = {
 };
 
 export default function Editor(props: Props) {
-  const { avro, json, changeJson, changeAvro } = props;
+  const { json, changeJson } = props;
 
   return (
-    <div>
-      <h1>JSON</h1>
-      <input
-        type="text"
-        value={json.value}
-        onChange={({ target }) => changeJson(target.value)}
-      />
-
-      <h1>AVRO</h1>
-      <input
-        type="text"
-        value={avro.value}
-        onChange={({ target }) => changeAvro(target.value)}
-      />
-    </div>
+    <Layout className={styles.layout}>
+      <Header>
+        <Menu theme="dark" mode="horizontal">
+          <Menu.Item key="1">Javro</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <div className={styles['layout-content']}>
+          <Row>
+            <Col span={12}>
+              <h1>JSON</h1>
+              <input
+                type="text"
+                value={json.value}
+                onChange={({ target }) => changeJson(target.value)}
+              />
+            </Col>
+            <Col span={12}>
+              <p>Bonjour</p>
+            </Col>
+          </Row>
+        </div>
+      </Content>
+    </Layout>
   );
 }
